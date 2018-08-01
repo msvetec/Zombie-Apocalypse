b7316af1445bc7ebcded44037382ca9227e293a4
Mario Svetec <mario.svetec@student.uniri.hr>
2018-08-02 01:48:02 +0200

    Car done
    Added car model ready to use, fixed some sound bugs

---
+++ Zombie-Apocalypse/Assets/Scripts/Car/EnterVehcle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
+
+public class EnterVehcle : MonoBehaviour {
+
+    public GameObject camera;
+    public GameObject player;
+    public GameObject exitTrigger;
+    public GameObject theCar;
+    private int triggerCheck;
+    private AudioSource audio;
+    private Rigidbody rb;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerCheck = 1;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        triggerCheck = 0;
+    }
+    private void Update()
+    {
+        if (triggerCheck == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                rb = theCar.GetComponent<Rigidbody>();
+                rb.constraints = RigidbodyConstraints.None;
+                theCar.transform.tag = "Player";
+                camera.SetActive(true);
+                player.SetActive(false);
+                audio = theCar.GetComponent<AudioSource>();
+                if (audio != null)
+                    audio.enabled = true;
+                
+                theCar.GetComponent<CarController>().enabled = true;
+                theCar.GetComponent<CarUserControl>().enabled = true;
+                theCar.GetComponent<CarAudio>().enabled = true;
+                exitTrigger.SetActive(true);
+                
+
+            }
+        }
+    }
+
+
+}
---
+++ Zombie-Apocalypse/Assets/Scripts/Car/ExitVehcle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class ExitVehcle : MonoBehaviour {
+
+    public GameObject camera;
+    public GameObject player;
+    public GameObject exitTrigger;
+    public GameObject theCar;
+    public GameObject exitPlace;
+    private Rigidbody rb;
+
+
+  
+    private void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.F))
+        {
+            rb = theCar.GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.FreezePositionX;
+            theCar.transform.tag = "Untagged";
+            camera.SetActive(false);
+            player.SetActive(true);
+            player.transform.position = exitPlace.transform.position;
+            theCar.GetComponent<CarController>().enabled = false;
+            theCar.GetComponent<CarUserControl>().enabled = false;
+            theCar.GetComponent<CarAudio>().enabled = false;
+            theCar.GetComponent<AudioSource>().enabled = false;
+            exitTrigger.SetActive(false);
+        }
+        
+    }
+
+}
